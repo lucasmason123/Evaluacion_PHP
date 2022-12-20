ea604467b9a5fee0ba7afef181765e6055d5dfb3
lucasmason123 <lucas.mason@inacapmail.cl>
2022-12-20 16:14:40 -0300

    Se agrega el proyecto

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstap CSS & JS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Sweetalert 2 CSS -->
+    <link rel="stylesheet" href="../css/sweetalert2.min.css">
+    <!-- CSS Personal -->
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.2.js" 
+    integrity="sha256-pkn2CUZmheSeyssYw3vMp1+xyub4m+e+QK4sQskvuo4=" crossorigin="anonymous"></script>
+
+    <title>Login PHP Bootstrap 5 2022</title>
+</head>
+<body>
+    
+</body>
+</html>
